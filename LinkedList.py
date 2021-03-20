3c4d11651fbe9b0d8265951a6e9f6b889ecaf521
Zachary Kiihne <zkiihne@gmail.com>
2021-03-20 16:49:39 -0400

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -57,7 +57,47 @@ def reverseList(self, head):
 # Merging two linked lists
 # Example: https://leetcode.com/problems/merge-two-sorted-lists/
 
-
+def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # defaults
+        if l1 == None:
+            return l2
+        elif l2 == None:
+            return l1
+        
+        # intial setup
+        if l1.val > l2.val:
+            head = l2
+            l2 = l2.next
+        else:
+            head = l1
+            l1 = l1.next
+        prevnode = head
+        
+        # go through lists
+        while (l1 != None) and (l2 != None):
+            
+            # conditioned comparison
+            if l1.val > l2.val:
+                prevnode.next = l2
+                prevnode = l2
+                l2 = l2.next
+            else:
+                prevnode.next = l1
+                prevnode = l1
+                l1 = l1.next
+        
+        # clean up
+        if l1 != None:
+            prevnode.next = l1
+        elif l2 != None:
+            prevnode.next = l2  
+            
+        return head
 
 
 
