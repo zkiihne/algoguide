70c132a943dec84153d554be77e8cfa8d1d96ce1
Zachary Kiihne <zkiihne@gmail.com>
2021-04-02 14:42:44 -0400

    Update Trees.py

--- Trees.py
+++ Trees.py
@@ -1,1 +1,22 @@
 
+
+
+# Example problem https://leetcode.com/problems/validate-binary-search-tree/submissions/
+def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def checkNode(node, constraintmin, constraintmax):
+            val = node.val
+            if val <= constraintmin or val >= constraintmax:
+                return False
+            if node.left != None and node.right != None:
+                return min(checkNode(node.left, constraintmin, val), checkNode(node.right, val, constraintmax))
+            elif node.left != None:
+                return checkNode(node.left, constraintmin, val)
+            elif node.right != None:
+                return checkNode(node.right, val, constraintmax)
+            else:
+                return True
+        return checkNode(root, float("-inf"), float("inf"))
